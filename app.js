2490318512a9082761cf324a685521c06fa4ac4f
Sunil Hatkadke <s.r.hatkadke@gmail.com>
2025-11-20 23:14:11 +0530

    Update CORS configuration to include PATCH method for enhanced API flexibility.

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ app.use(
       }
     },
     credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE"],
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
