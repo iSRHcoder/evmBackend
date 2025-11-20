83ec827c394395a78d69885d4c306b505e776fdc
Sunil Hatkadke <s.r.hatkadke@gmail.com>
2025-11-20 20:19:58 +0530

    Update CORS configuration to allow requests from the Netlify frontend in addition to the local development environment.

--- app.js
+++ app.js
@@ -13,12 +13,14 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:5173", // your Vite frontend
+    origin: [
+      "http://localhost:5173",
+      "https://vote-your-candidate.netlify.app",
+    ], // your Vite frontend
     credentials: true,
   })
 );
 
-app.use(cors());
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true }));
 app.use(helmet());
