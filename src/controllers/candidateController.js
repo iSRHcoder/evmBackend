507ed4c8ab8b058de48b6767aceff3fefa81e13b
Sunil Hatkadke <s.r.hatkadke@gmail.com>
2025-11-22 22:45:44 +0530

    Update candidate model to set 'multipleVotes' field default to false and adjust candidate creation logic to include this field.

--- src/controllers/candidateController.js
+++ src/controllers/candidateController.js
@@ -17,6 +17,7 @@ export const createCandidate = async (req, res) => {
       constituency,
       party,
       wardNo,
+      multipleVotes = false,
       electionDate,
     } = req.body;
 
@@ -78,6 +79,7 @@ export const createCandidate = async (req, res) => {
       symbolName,
       constituency,
       party,
+      multipleVotes,
       wardNo,
       electionDate: electionDate || null,
       candidatePhoto,
